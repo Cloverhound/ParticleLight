34b435b7d027cc94d243dada934f8f41928b54a5
AustinLutz <austingrierlutz@gmail.com>
2016-07-07 15:37:47 -0400

    Added support for blinking

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/light/on' => 'light#on'
   get '/light/off' => 'light#off'
-
+  get '/light/blink' => 'light#blink'
 end
